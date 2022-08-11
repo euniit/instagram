f93796a0ca1487830398204998c0bc1c48ef3942
euniit <1526374859o@gmail.com>
2022-08-11 18:17:40 +0900

    Person.tsx 구현
    콜백 함수 사용법 익히기

--- src/components/TouchableView.tsx
+++ src/components/TouchableView.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import type {FC, ReactNode, ComponentProps} from 'react'
+import type {FC, ComponentProps} from 'react'
 import {TouchableOpacity, View} from 'react-native'
+import type {StyleProp, ViewStyle} from 'react-native'
 
 type TouchableOpacityProps = ComponentProps<typeof TouchableOpacity>
 
-export type TouchableViewProps = TouchableOpacityProps
+export type TouchableViewProps = TouchableOpacityProps & {
+    viewStyle?: StyleProp<ViewStyle>
+}
 
 // prettier-ignore
 export const TouchableView: FC<TouchableViewProps> = ({
